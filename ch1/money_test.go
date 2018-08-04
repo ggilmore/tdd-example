056c198b1ffed3ea47efc06dc0ea1017a2b9fb23
Geoffrey Gilmore <ggilmore@mit.edu>
2018-08-03 23:40:36 -0700

    ch1: failing side effect test

--- ch1/money_test.go
+++ ch1/money_test.go
@@ -4,8 +4,14 @@ import "testing"
 
 func Test_Multiplication(t *testing.T) {
 	five := Dollar{Amount: 5}
-	five.Times(2)
-	if five.Amount != 10 {
-		t.Errorf("expected $5*2 = $10, got: %d", five.Amount)
+	product := five.Times(2)
+
+	if product != 10 {
+		t.Errorf("expected $5*2 = $10, got: %d", product)
+	}
+
+	product := five.Times(3)
+	if product != 15 {
+		t.Errorf("expected $5*3 = $15, got: %d", product)
 	}
 }
